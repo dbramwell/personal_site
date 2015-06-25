e47548f623a02327d45b7cc4bb15ccf58ceade38
David <david.j.bramwell@googlemail.com>
2015-06-26 00:52:14 +0100

    Fix deployment to heroku

--- site/scripts/deploy_to_heroku.sh
+++ site/scripts/deploy_to_heroku.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-cd ../app
-test "$(ls | grep .git)" == "" && git init
-test "$(ls | grep .git)" == "" && git remote add heroku https://git.heroku.com/david-bramwell.git
-ls | grep -v node_modules | xargs git add
-git commit -m "Adding app"
-git push origin heroku
+git remote add heroku https://git.heroku.com/david-bramwell.git
+git subtree push --prefix site/app heroku master
