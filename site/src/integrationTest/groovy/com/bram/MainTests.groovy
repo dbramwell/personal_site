86b9c991ea4a104cc453978f458ff5a528bed0b8
David <david.j.bramwell@googlemail.com>
2015-06-28 23:12:23 +0100

    Moved integration test, fixed deployment

---
+++ site/src/integrationTest/groovy/com/bram/MainTests.groovy
@@ -0,0 +1,33 @@
+package com.bram
+
+import java.util.concurrent.TimeUnit
+
+import org.junit.*
+
+import static org.junit.Assert.*
+
+import org.openqa.selenium.*
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+
+class MainTests {
+    WebDriver driver
+    final baseUrl = "http://localhost:5000"
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver()
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS)
+    }
+
+    @Test
+    public void testBaseURLHasUnCollapsedNavBar() throws Exception {
+        driver.get(baseUrl)
+        assertFalse(driver.findElement(By.className("navbar")).getAttribute('class').contains('top-nav-collapse'))
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit()
+    }
+}
--- site/src/scripts/deploy_to_heroku.sh
+++ site/src/scripts/deploy_to_heroku.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
-git remote add heroku https://git.heroku.com/david-bramwell.git
-git subtree push --prefix site/app heroku master
+#!/bin/bash
+cd $1
+if [[ "$(git remote)" != *"heroku"* ]]
+then
+   git remote remove heroku https://git.heroku.com/david-bramwell.git
+   git remote add heroku https://git.heroku.com/david-bramwell.git
+fi
+git push heroku `git subtree split --prefix site/src/app master`:master --force
